f5000595e219227d2ab305a53956c194f49d8075
Jimmy Zhang <jimmyjz1127@gmail.com>
2024-03-19 15:37:06 +0000

    Added attention weight visual to UI

--- src/client/src/Components/Result/Result.jsx
+++ src/client/src/Components/Result/Result.jsx
@@ -4,14 +4,18 @@ import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Axios from 'axios';
 
+import AttentionVisual from "./AttentionVisual";
+
 import "./Result.css"
 
 import EyeIcon from './../../Assets/eye.png'
+import InsightIcon from './../../Assets/insight.png'
 
 function Result(props){
     const {data, index} = props;
 
     const [showDoc, setShowDoc] = useState(false);
+    const [showInsights, setShowInsights] = useState(false);
 
     return (
         <div className="result-wrapper flex col align-center">
@@ -22,10 +26,18 @@ function Result(props){
                     <text>Court : {data.court}</text>
                     <text>Similarity Score : <text className="highlight">{data.similarity}</text></text>
                 </div>
-                <button className="view-btn" onClick={(e) => setShowDoc(!showDoc)}>
-                    View
-                </button>
+                <div className="flex col align-end">
+                    <button className="view-btn" onClick={(e) => setShowDoc(!showDoc)}>
+                        Document <img src={EyeIcon}/>
+                    </button>
+                    <button className="view-btn" onClick={(e) => setShowInsights(!showInsights)}>
+                        Insights <img src={InsightIcon}/>
+                    </button>
+                </div>
             </div>
+            {showInsights &&
+                <AttentionVisual attentionData={JSON.parse(data.attention)} query={JSON.parse(data.query_tokens)}/>
+            }
             {showDoc &&
                 <div className="doc-viewer">
                     {data.main}
